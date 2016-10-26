2496442ba9a53ee67ff2cf5445b99fffd122d70e
hieuvo <trunghieusgn@gmail.com>
2016-10-26 06:31:24 -0400

    clean up

--- config-client/src/main/java/com/example/ConfigRest.java
+++ config-client/src/main/java/com/example/ConfigRest.java
@@ -1,16 +1,27 @@
 package com.example;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigRest {
     @Value("${hello}")
     String hello;
+
+    @Value("${welcome}")
+    String welcome;
 
     @RequestMapping("/hello")
     public String hello() {
         return hello;
     }
+
+    @RequestMapping("/welcome")
+    public String welcome() {
+        return welcome;
+    }
+
 }
